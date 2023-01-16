54885a87abb6bc5c4aa034155d9cf28add2b207d
mtownsend509 <mgtownsend509@gmail.com>
2023-01-15 18:48:07 -0800

    commented out everything that was breaking models and seeds, and added bcrypt dependency

--- Utils/Auth.js
+++ Utils/Auth.js
@@ -1,6 +1,6 @@
 const withAuth = (req, res, next) => {
       // If the user isn't logged in, redirect them to the login route
-      if (!req.session.user_id) {
+      if (!req.session.profile_id) {
           res.redirect('/login');
       } else {
           next();
--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,15 +1,18 @@
 const seedPlants = require('./plant-seed');
 const seedProfiles = require('./profile-seed');
 
-const sequelize = require('../config/connection');
+const { Plant } = require('../Models');
+const { Profile } = require('../Models');
+
+const sequelize = require('../config/Connection');
 
 const seedTables = async () => {
     await sequelize.sync({force: true});
     console.log('\n----- DATABASE SYNCED -----\n');
-    await seedPlants();
-    console.log('\n----Plants Seeded -------\n');
     await seedProfiles();
     console.log('\n----Profiles Seeded -------\n');
+    await seedPlants();
+    console.log('\n----Plants Seeded -------\n');
 
     process.exit(0);
 };
