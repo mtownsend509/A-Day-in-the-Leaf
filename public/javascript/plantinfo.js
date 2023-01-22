c069e4c88ef54d86de3d255b89d0a65e9dfacc52
Abigail Doyle <abigaildoyle3@gmail.com>
2023-01-21 22:37:30 -0800

    begin basic switch case

---
+++ public/javascript/plantinfo.js
@@ -0,0 +1,19 @@
+const plantType = {whateverthevariableis}
+
+switch(plantType) {
+      case "houseplant": houseplantImage();
+      break;
+      case "cactus": cactusImage();
+      break;
+      case "fern": fernImage();
+      break;
+      case "flowering": floweringImage();
+      break;
+      default: houseplantImage();
+      break;
+}
+
+const houseplantImage = () {
+      var planticonimage = document.getElementById("planticon");
+      $
+}
